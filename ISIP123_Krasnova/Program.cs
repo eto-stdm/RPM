fe58cdf13c40b201ef0d32a0925003467dc2b0ab
eto-stdm <potatocuteuwu@gmail.com>
2025-09-22 19:27:33 +0300

    добавление тестовых товаров, проверок, правка меню и add_Product(). написание call_for_delivery(), sell_Product(), search_for_Product

--- ISIP123_Krasnova/Program.cs
+++ ISIP123_Krasnova/Program.cs
@@ -1,11 +1,13 @@
-﻿int id = 4;
+﻿int id = 5;
 string n = "0";
 
 List<Product> list_prod = new List<Product>();
 
 Product p1 = new Product(0, "Бананы", 50, 20, true, "Овощи и фрукты");
-Product p2 = new Product(1, "Молоко", 100, 50, false, "Молочные продукты");
-Product p3 = new Product(2, "Пицца", 200, 10, false, "Хлебобулочные изделия");
+Product p2 = new Product(1, "Молоко", 100, 0, false, "Молочные продукты");
+Product p3 = new Product(2, "Пицца", 200, 10, true, "Хлебобулочные изделия");
+Product p4 = new Product(3, "Йогурт", 40, 100, true, "Молочные продукты");
+Product p5 = new Product(4, "Салат листовой", 30, 0, false, "Овощи и фрукты");
 
 list_prod.Add(p1);
 list_prod.Add(p2);
@@ -18,8 +20,8 @@ do
     Console.WriteLine("1 - Вывод списка с товарами");
     Console.WriteLine("2 - Добавление товара");
     Console.WriteLine("3 - Удаление товара (по ID)");
-    Console.WriteLine("4 - Продажа товара");
-    Console.WriteLine("5 - Заказ поставки товара");
+    Console.WriteLine("4 - Заказ поставки товара (по ID)");
+    Console.WriteLine("5 - Продажа товара (по ID)");
     Console.WriteLine("6 - Поиск товара (по ID, названию или категории)");
     Console.WriteLine("0 - Выход");
     Console.WriteLine("-----------------------------");
@@ -29,7 +31,7 @@ do
     {
         case "0": break;
         case "1": print_for_each(list_prod); break;
-        case "2": add_Product(list_prod, id); break;
+        case "2": id = add_Product(list_prod, id); break;
         case "3": remove_Product(list_prod); break;
         case "4": call_for_delivery(); break;
         case "5": sell_Product(); break;
@@ -39,19 +41,16 @@ do
     }
 } while (n != "0");
 
-
-
 void print_for_each(List<Product> list_prod)
 {
     foreach (Product p in list_prod)
     {
         p.PrintInfo();
     }
-}
+}// готово
 
 int add_Product(List<Product> list_prod, int id)
 {
-   
     string temp_ = "";
     string temp2_ = "";
 
@@ -68,6 +67,7 @@ int add_Product(List<Product> list_prod, int id)
         Console.WriteLine("Ошибка: Имя не может быть пустым");
         return id;
     }
+
     Console.WriteLine("Напишите цену продукта:");
     price = Convert.ToInt32(Console.ReadLine());
     if (price < 0)
@@ -75,6 +75,7 @@ int add_Product(List<Product> list_prod, int id)
         Console.WriteLine("Ошибка: Цена не может быть отрицательной");
         return id;
     }
+
     Console.WriteLine("Напишите количество продукта:");
     quantity = Convert.ToInt32(Console.ReadLine());
     if (quantity < 0)
@@ -100,12 +101,12 @@ int add_Product(List<Product> list_prod, int id)
     else if (temp2_ == "3") { category = "Овощи и фрукты"; }
     else { Console.WriteLine("Ошибка: Некорректная категория"); return id; }
 
-    id++;
     Product add = new Product(id, name, price, quantity, isleft, category);
     list_prod.Add(add);
     Console.WriteLine($"Продукт {add.name} добавлен");
-    return 0;
-}
+    id++;
+    return id;
+}// готово*
 
 void remove_Product(List<Product> list_prod)
 {
@@ -119,40 +120,123 @@ void remove_Product(List<Product> list_prod)
             Console.WriteLine($"Товар {p.name} с ID {p.productID} удалён");
             list_prod.Remove(p);
         }
+        else
+        {
+            Console.WriteLine("Ошибка: Товара с таким ID не существует");
+        }
+        break;
     }
-}
+}// готово
 
 void call_for_delivery()
 {
+    string call;
+    Console.Write("Какой продукт вы хотите заказать?: ");
+    call = Console.ReadLine();
 
-}
+    foreach (Product p in list_prod)
+    {
+        if (p.productID == Convert.ToInt32(call))
+        {
+            Console.WriteLine($"Сколько продукта {p.name} вы хотите заказать?");
+            Console.WriteLine($"У вас в наличии {p.quantity} шт.");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            p.quantity += count;
+            Console.WriteLine($"{p.name} {count} шт. заказано.");
+
+            if (p.quantity > 0) { p.isleft = true; }
+            break;
+        }
+    }
+}// готово*
 
 void sell_Product()
 {
+    string sell;
+    Console.Write("Какой продукт вы хотите продать?: ");
+    sell = Console.ReadLine();
 
-}
+    foreach (Product p in list_prod)
+    {
+        if (p.productID == Convert.ToInt32(sell))
+        {
+            Console.WriteLine($"Сколько продукта {p.name} вы хотите продать?");
+            Console.WriteLine($"У вас в наличии {p.quantity} шт.");
+            int count = Convert.ToInt32(Console.ReadLine());
+            if (count <= p.quantity)
+            {   p.quantity -= count;
+                Console.WriteLine($"{p.name} {count} шт. продано.");
+            }
+            else { Console.WriteLine("Ошибка: У вас недостаточно товара для продажи."); }
+
+            if (p.quantity == 0) { p.isleft = false; }
+            break;
+        }
+    }
+
+}// готово*
 
 void search_for_Product(List<Product> list_prod)
 {
-    Console.Write("Введите часть названия продукта для поиска: ");
+    Console.WriteLine("Выберите, как вы хотите найти товар:");
+    Console.WriteLine("1 - По коду");
+    Console.WriteLine("2 - По названию");
+    Console.WriteLine("3 - По категории");
     string search = Console.ReadLine();
-
-    search = search.ToLower();
-    int counter = 0;
-    foreach (Product p in list_prod)
+    
+    switch (search)
     {
-        if (p.name.ToLower().Contains(search))
-        {
-            Console.WriteLine(p);
-            counter++;
-        }
-    }
-    if (counter == 0)
-    {
-        Console.WriteLine("Продукт не найден");
-    }
-}
+        case "1":
+            Console.Write("Введите ID для поиска: ");
+            search = Console.ReadLine();
+            int counter = 0;
+            foreach (Product p in list_prod)
+            {
+                if (p.productID == Convert.ToInt32(search))
+                {
+                    p.PrintInfo();
+                    counter++;
+                }
+            }
+            if (counter == 0) { Console.WriteLine("Ошибка: Продукт не найден"); }
+            break;
 
+        case "2":
+            Console.Write("Введите часть названия продукта для поиска: ");
+            search = Console.ReadLine();
+            search = search.ToLower();
+            counter = 0;
+            foreach (Product p in list_prod)
+            {
+                if (p.name.ToLower().Contains(search))
+                {
+                    p.PrintInfo();
+                    //Console.WriteLine(p);
+                    counter++;
+                }
+            }
+            if (counter == 0) { Console.WriteLine("Ошибка: Продукт не найден");}
+            break;
+
+        case "3":
+            Console.Write("Введите категорию для поиска: ");
+            search = Console.ReadLine();
+            counter = 0;
+            foreach (Product p in list_prod)
+            {
+                if (p.category.Contains(search))
+                {
+                    p.PrintInfo();
+                    counter++;
+                }
+            }
+            if (counter == 0) { Console.WriteLine("Ошибка: Продукт не найден"); }
+            break;
+
+        default: Console.WriteLine("Ошибка: Неверно набранная категория"); break;
+    }
+}// готово*
 
 class Product
 {
@@ -161,7 +245,7 @@ class Product
     public int price;
     public int quantity;
     public bool isleft;
-    public string category; // сделать минимум 3 варианта
+    public string category;
 
     public Product(int productID, string name, int price, int quantity, bool isleft, string category)
     {
@@ -184,4 +268,4 @@ class Product
         Console.WriteLine($"Категория: {category}");
         Console.WriteLine("*************************");
     }
-}
+}// готово
