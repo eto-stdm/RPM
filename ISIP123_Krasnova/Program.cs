2ca167da6b254b289b7acb91ec22db03a01df25e
eto-stdm <potatocuteuwu@gmail.com>
2025-09-22 12:49:44 +0300

    добавлены add_Product() и remove_Product()

--- ISIP123_Krasnova/Program.cs
+++ ISIP123_Krasnova/Program.cs
@@ -1,27 +1,4 @@
-﻿//Создайте консольное приложение C# для учёта товаров в магазине. 
-
-//У товара должны быть следующее параметры:
-//Уникальный код(начинается с "1", должен автоматически ставиться при пополнении списка товаров)
-//Название
-//Цена
-//Количество
-//Остался ли ещё товар на складе
-//Категория (выбирается из имеющихся, задаются в коде, сделайте как минимум 3)
-
-//Мы можем работать с товаром через команды:
-//Добавить товар
-//Удалить товар
-//Заказать поставку товара
-//Продать товар
-//Поиск товаров (по коду, названию и категории). Необходимо выводить полную информацию о товаре.
-
-//Для выполнения задания используйте все возможности языка C#, изученные ранее (классы, списки, перечисления и так далее).
-//Обязательно заполните список товаров пятью тестовыми данными.Обязательно сделайте проверку всевозможных вводимых значений
-//(не должно быть возможности создать пустой товар, с отрицательной ценой, с отрицательным количеством).Программа не должна вылетать
-//в процессе работы.Программа должна выводить информацию в чётком и ясном виде для пользователя.При продаже товара, обязательно
-//сделайте проверку остатка на складе.Не забудьте отправлять код по частям, разными коммитами, и делать комментарии к коммитам
-//осмысленные.
-
+﻿int id = 0;
 
 //Product avocado = new Product(1, "Авокадо", 60, 4);
 
@@ -60,40 +37,68 @@
 //    }
 //}
 
-//void add_Product(List<Product> list_prod)
-//{
-//    int id = 0;
-//    string na = "";
-//    int pr = 0;
-//    int qu = 0;
-//    Console.WriteLine("Напишите ID продукта:");
-//    id = Convert.ToInt32(Console.ReadLine());
-//    Console.WriteLine("Напишите наименование продукта:");
-//    na = Console.ReadLine();
-//    Console.WriteLine("Напишите цену продукта:");
-//    pr = Convert.ToInt32(Console.ReadLine());
-//    Console.WriteLine("Напишите количество продукта:");
-//    qu = Convert.ToInt32(Console.ReadLine());
+void call_for_delivery()
+{
 
-//    Product add = new Product(id, na, pr, qu);
-//    list_prod.Add(add);
-//    Console.WriteLine($"Продукт {add.name} добавлен");
-//}
+}
 
-//void remove_Product(List<Product> list_prod)
-//{
-//    int id;
-//    Console.WriteLine("Напишите ID продукта:");
-//    id = Convert.ToInt32(Console.ReadLine());
-//    foreach (Product p in list_prod.ToList())
-//    {
-//        if (p.productID == id)
-//        {
-//            Console.WriteLine($"Товар {p.name} с ID {p.productID} удалён");
-//            list_prod.Remove(p);
-//        }
-//    }
-//}
+void add_Product(List<Product> list_prod, int id)
+{
+    string temp_ = "";
+    string temp2_ = "";
+
+    string name = "";
+    int price = 0;
+    int quantity = 0;
+    bool isleft = false;
+    string category = "";
+
+    Console.WriteLine("Напишите наименование продукта:");
+    name = Console.ReadLine();
+    Console.WriteLine("Напишите цену продукта:");
+    price = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Напишите количество продукта:");
+    quantity = Convert.ToInt32(Console.ReadLine());
+    do
+    {
+        Console.WriteLine("Напишите есть ли продукт на складе: (да или нет)");
+        temp_ = Console.ReadLine();
+        temp_.ToLower();
+        if (temp_ == "да") { isleft = true; }
+        if (temp_ == "нет") { isleft = false; }
+    } while( temp_ != "да" || temp_ != "нет");
+    do
+    {
+        Console.WriteLine("Выберите категорию продукта:");
+        Console.WriteLine("1 - Молочные продукты");
+        Console.WriteLine("2 - Хлебобулочные изделия");
+        Console.WriteLine("3 - Овощи и фрукты");
+        temp2_ = Console.ReadLine();
+        if (temp2_ == "1") { category = "Молочные продукты"; }
+        if (temp2_ == "2") { category = "Хлебобулочные изделия"; }
+        if (temp2_ == "3") { category = "Овощи и фрукты"; }
+    } while (temp2_ != "1" || temp2_ != "2" || temp2_ != "3");
+
+    id++;
+    Product add = new Product(id, name, price, quantity, isleft, category);
+    list_prod.Add(add);
+    Console.WriteLine($"Продукт {add.name} добавлен");
+}
+
+void remove_Product(List<Product> list_prod)
+{
+    int id;
+    Console.WriteLine("Напишите ID продукта:");
+    id = Convert.ToInt32(Console.ReadLine());
+    foreach (Product p in list_prod.ToList())
+    {
+        if (p.productID == id)
+        {
+            Console.WriteLine($"Товар {p.name} с ID {p.productID} удалён");
+            list_prod.Remove(p);
+        }
+    }
+}
 
 class Product
 {
@@ -104,12 +109,13 @@ class Product
     public bool isleft;
     public string category; // сделать минимум 3 варианта
 
-    public Product(int productID, string name, int price, int quantity, string category)
+    public Product(int productID, string name, int price, int quantity, bool isleft, string category)
     {
         this.productID = productID;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.isleft = isleft;
         this.category = category;
     }
 
