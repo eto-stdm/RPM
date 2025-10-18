265510339fb6ed5097bf38b663787e182bdad172
eto-stdm <potatocuteuwu@gmail.com>
2025-10-18 19:01:56 +0300

    заполнены данные предметов, написан start()

--- ISIP123_Krasnova/Program.cs
+++ ISIP123_Krasnova/Program.cs
@@ -1,29 +1,50 @@
 ﻿using System;
 using System.Xml.Linq;
 
-//List<string> rewards = new List<string>() { "Четырёхлопастный винт", "Дизельный двигатель MB 517 V12" };
-//List<string> weapons = new List<string>() { "КПВТ" };
-//List<string> armors = new List<string>() { "Голубая накидка" };
-
-
-//Player cubeguy = new Player("Кубочел", 100, 5, 10, weapons[0], armors[0]);
-
-//Common skebob = new Common("Скебоб", 20, 4, 0);
-//Common mouse = new Common("Мыш (компьютерная)", 40, 8, 2);
-
-//Boss apache = new Boss("AH-64 «Apache»", 1000, 100, 10, rewards[0]);
-//Boss maus = new Boss("Maus", 1000, 100, 50, rewards[1]);
-
 int turn_count = 1;
+string fumo = "\r\n\r\n                                                                                                    \r\n ...........................:...................................................................::. \r\n .................................:..................:.....:...:..-.:-:...::::.:::::.:............. \r\n .........:.................::.:.::.:..:..:...........::::...:::.:.......................:::......: \r\n ..............::...:...:.....................:..::.:......:........::..........   ...-:::.......:. \r\n .................    ...........::.:-.....:..:.......:....:...............:.....@*   ........::.:. \r\n ..........::..   .@@*=- ........::.......:..........:...:.........:........:-.  @@@@@   .::-:.::.- \r\n ......:...   *@@+.   .....:::..............:.....:....:....::..:.....:....... .-.. .#@@@   ..:.:.: \r\n .......   @@%.         .....:::..:..:..:..:..:.......:.:.-...:...........:...-..       .%@@   -::- \r\n .:... .%@#     . @@@@@-...-...:............:....:................:....... ..+.:@@@@@@@@.  +@@. ... \r\n ..  :@@    .....       ........::::........:..........:..::..:.....:... .-+-:+.        .... .*@=   \r\n   ..                   ........:....:.:..........:.........::.:....   ++=-::...:: .                \r\n .@@@@@@@@@@@@@@@@@@@@=           .. ...:......:..:..:.....:...      ..              *@@@@@@@@@@@@@*\r\n @@@.      @* .    ..+@@@@@@@@@@@@ .:.:......:....:...:.......-=@@@@@@@@@@@@@@@@@@@@@@@@+:.      @  \r\n # .@@@@@@@#--*-##*%+**===*===--@@ ......:.............:=----=: .@@        @%#**@+**#**+*+****:-@@  \r\n :. :@:..*=+=:*::+===*++#:-:-..@@  ............................. @@@@@@@@@@#=+==*+#*%*=#@**+--+@@   \r\n ..   @@@#.  .+%-:=..==:*  .@@@@  ..............................   @@@*---***+##=#++==+-*-.#@@@#    \r\n :.       @@@@@@@@@@@@@@@@@@=    ................................    :@@@@@@@@@*@@@@@@@@@@@@     .= \r\n +*......                     ...........................:....:...:.         @@@@@@.         ....:= \r\n +**+.......................................................................        ..........:--*+ \r\n    :-=......................................................................................:----- \r\n.@@=        .........................*@.:..           ....-.@.............::....::........--==+-.   \r\n =*@@@@@@@#       ......................:..@#@@@@@@@@:% : ...........................:.         .*%.\r\n @@@@@@##@@@@@#*@#.---:::::.:::-:.                     ....................::..::::-...@@@@@@@@=  @ \r\n ..  ..%@:..:@@@@        ....-.   .%@@@@@@@@@#...                        .-*+++--:.            .@@@ \r\n  .#@.   ..      #@@@@@        %@@@@@#****#%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+         *@@@@@@@%=+=.    \r\n                      .=@@@@@@@+*                                       .=@@@@@@@@#:      .         \r\n\r\n";
+
+Wearable i_weapon_def = new Wearable(Type_e.Weapon, "Меч героя", "Стандартное оружие персонажа. +3 к атаке", 3);
+Wearable i_armor_def = new Wearable(Type_e.Armor, "Броня героя", "Стандартная броня персонажа. +3 к защите", 3);
+
+Item i_heal = new Item (Type_e.Heal, "Зелье лечения", "Мгновенно лечит вас!");
+Wearable i_weapon_1 = new Wearable(Type_e.Weapon, "Меч цветов", "+4 к атаке", 4);
+Wearable i_weapon_2 = new Wearable(Type_e.Weapon, "Клинок света", "+5 к атаке", 5);
+Wearable i_weapon_3 = new Wearable(Type_e.Weapon, "Огненая булава", "+6 к атаке", 6);
+Wearable i_weapon_4 = new Wearable(Type_e.Weapon, "Теневой кинжал", "+7 к атаке", 7);
+Wearable i_weapon_5 = new Wearable(Type_e.Weapon, "Коготь тьмы", "+8 к атаке", 8);
+Wearable i_weapon_6 = new Wearable(Type_e.Weapon, "Нож хаоса", "+10 к атаке", 10);
+Wearable i_weapon_non = new Wearable(Type_e.Weapon, "Меч имба", "Имба", 100);
+Wearable i_armor_1 = new Wearable(Type_e.Armor, "Кленовый костюм", "+4 к защите", 4);
+Wearable i_armor_2 = new Wearable(Type_e.Armor, "Кираса солнца", "+5 к защите", 5);
+Wearable i_armor_3 = new Wearable(Type_e.Armor, "Железный панцирь", "+6 к защите", 6);
+Wearable i_armor_4 = new Wearable(Type_e.Armor, "Обсидиановая броня", "+7 к защите", 7);
+Wearable i_armor_5 = new Wearable(Type_e.Armor, "Доспехи рыцаря", "+8 к защите", 8);
+Wearable i_armor_6 = new Wearable(Type_e.Armor, "Облачение богов", "+10 к защите", 10);
+
+Item[] items = { i_heal, i_weapon_1, i_weapon_2, i_weapon_3, i_weapon_4, i_weapon_5, i_weapon_6,
+                 i_armor_1, i_armor_2, i_armor_3, i_armor_4, i_armor_5, i_armor_6 };
+
+
+
+List<Enemy> commons = new List<Enemy> { };
+List<Enemy> bosses = new List<Enemy> { };
+
+Player play = start();
 
 do
 {
-
+    break;
 } while (true);
 
-void start()
+Player start()
 {
-
+    Console.WriteLine(fumo);
+    Console.Write("Выберите имя игрока: ");
+    string n = Console.ReadLine();
+    Player play = new Player(n, 100, i_weapon_def.Num, i_armor_def.Num, i_weapon_def, i_armor_def);
+    play.Print();
+    return play;
 }
 
 //Сделайте так, чтобы все шансы и случайные величины
@@ -44,6 +65,12 @@ void turn()
 
 void fight(bool is_boss)
 {
+    if (is_boss)
+    {
+        Random rnd = new Random();
+
+        //Console.WriteLine($"Вы встретили босса {}!");
+    }
 //    -Игрок всегда ходит первым.
 //- Ход игрока: выбрать Атаку или Защиту.
 
@@ -59,6 +86,8 @@ void fight(bool is_boss)
 
 void chest()
 {
+    Console.WriteLine("Вы наткнтулись на сундук");
+
 //    Из сундука может выпасть лечебное зелье,
 //оружие или доспех(случайно).
 
@@ -70,6 +99,29 @@ void chest()
 //-Дать выбор: взять новый предмет(заменив текущий)
 //или выбросить его.
 }
+enum Type_e { Heal, Weapon, Armor }
+class Item
+{
+    public Type_e Type;
+    public string Name;
+    public string Description;
+
+    public Item(Type_e type, string name, string description)
+    {
+        Type = type; Name = name; Description = description;
+    }
+}
+
+class Wearable : Item
+{
+    public int Num;
+    public Wearable(Type_e type, string name, string description, int num)
+        : base(type, name, description)
+    {
+        Num = num;
+    }
+}
+
 
 class Player
 {
@@ -77,9 +129,9 @@ class Player
     public int hp;
     public int attack;
     public int defense;
-    public string weapon;
-    public string armor;
-    public Player(string name, int hp, int attack, int defense, string weapon, string armor)
+    public Item weapon;
+    public Item armor;
+    public Player(string name, int hp, int attack, int defense, Item weapon, Item armor)
     {
         this.name = name;
         this.hp = hp;
@@ -88,13 +140,14 @@ class Player
         this.weapon = weapon;
         this.armor = armor;
     }
-    public virtual void Print()
+    public void Print()
     {
         Console.WriteLine($"Имя: {name}");
         Console.WriteLine($"Здоровье: {hp}");
         Console.WriteLine($"Урон: {attack}");
-        Console.WriteLine($"Оружие: {weapon}");
-        Console.WriteLine($"Броня: {armor}");
+        Console.WriteLine($"Защита: {defense}");
+        Console.WriteLine($"Оружие: {weapon.Name}");
+        Console.WriteLine($"Броня: {armor.Name}");
     }
 }
 class Enemy
