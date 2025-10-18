19dcf6a0b370e6abba6d14bf32588dae7a37fd5a
eto-stdm <potatocuteuwu@gmail.com>
2025-10-18 20:45:07 +0300

    дописаная функция chest() с нерабочим восполнением hp при 1 предмете в списке

--- ISIP123_Krasnova/Program.cs
+++ ISIP123_Krasnova/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Xml.Linq;
+// maglin - gob + mag
 
-int turn_count = 1;
+int room_count = 1;
+int boss_count = 0;
 string fumo = "\r\n\r\n                                                                                                    \r\n ...........................:...................................................................::. \r\n .................................:..................:.....:...:..-.:-:...::::.:::::.:............. \r\n .........:.................::.:.::.:..:..:...........::::...:::.:.......................:::......: \r\n ..............::...:...:.....................:..::.:......:........::..........   ...-:::.......:. \r\n .................    ...........::.:-.....:..:.......:....:...............:.....@*   ........::.:. \r\n ..........::..   .@@*=- ........::.......:..........:...:.........:........:-.  @@@@@   .::-:.::.- \r\n ......:...   *@@+.   .....:::..............:.....:....:....::..:.....:....... .-.. .#@@@   ..:.:.: \r\n .......   @@%.         .....:::..:..:..:..:..:.......:.:.-...:...........:...-..       .%@@   -::- \r\n .:... .%@#     . @@@@@-...-...:............:....:................:....... ..+.:@@@@@@@@.  +@@. ... \r\n ..  :@@    .....       ........::::........:..........:..::..:.....:... .-+-:+.        .... .*@=   \r\n   ..                   ........:....:.:..........:.........::.:....   ++=-::...:: .                \r\n .@@@@@@@@@@@@@@@@@@@@=           .. ...:......:..:..:.....:...      ..              *@@@@@@@@@@@@@*\r\n @@@.      @* .    ..+@@@@@@@@@@@@ .:.:......:....:...:.......-=@@@@@@@@@@@@@@@@@@@@@@@@+:.      @  \r\n # .@@@@@@@#--*-##*%+**===*===--@@ ......:.............:=----=: .@@        @%#**@+**#**+*+****:-@@  \r\n :. :@:..*=+=:*::+===*++#:-:-..@@  ............................. @@@@@@@@@@#=+==*+#*%*=#@**+--+@@   \r\n ..   @@@#.  .+%-:=..==:*  .@@@@  ..............................   @@@*---***+##=#++==+-*-.#@@@#    \r\n :.       @@@@@@@@@@@@@@@@@@=    ................................    :@@@@@@@@@*@@@@@@@@@@@@     .= \r\n +*......                     ...........................:....:...:.         @@@@@@.         ....:= \r\n +**+.......................................................................        ..........:--*+ \r\n    :-=......................................................................................:----- \r\n.@@=        .........................*@.:..           ....-.@.............::....::........--==+-.   \r\n =*@@@@@@@#       ......................:..@#@@@@@@@@:% : ...........................:.         .*%.\r\n @@@@@@##@@@@@#*@#.---:::::.:::-:.                     ....................::..::::-...@@@@@@@@=  @ \r\n ..  ..%@:..:@@@@        ....-.   .%@@@@@@@@@#...                        .-*+++--:.            .@@@ \r\n  .#@.   ..      #@@@@@        %@@@@@#****#%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+         *@@@@@@@%=+=.    \r\n                      .=@@@@@@@+*                                       .=@@@@@@@@#:      .         \r\n\r\n";
 
 Wearable i_weapon_def = new Wearable(Type_e.Weapon, "Меч героя", "Стандартное оружие персонажа. +3 к атаке", 3);
@@ -22,15 +24,60 @@ Wearable i_armor_4 = new Wearable(Type_e.Armor, "Обсидиановая бро
 Wearable i_armor_5 = new Wearable(Type_e.Armor, "Доспехи рыцаря", "+8 к защите", 8);
 Wearable i_armor_6 = new Wearable(Type_e.Armor, "Облачение богов", "+10 к защите", 10);
 
-Item[] items = { i_heal, i_weapon_1, i_weapon_2, i_weapon_3, i_weapon_4, i_weapon_5, i_weapon_6,
+Goblin gob1 = new Goblin("Гоблин с мечом", 25, 2, 1.5, 10);
+Goblin gob2 = new Goblin("Гоблин с кувалдой", 20, 4, 1, 10);
+Goblin gob3 = new Goblin("Гоблин в лодке", 30, 1, 4, 10);
+Goblin gob_boss = new Goblin("Большой гоблин", (25 * 2), (2 * 1.5), (2 * 1.2), (10 * 2));
+Skeleton skel1 = new Skeleton("Скелет с луком", 20, 3, 1.5, true);
+Skeleton skel2 = new Skeleton("Скелет с арбалетом", 30, 4, 2, true);
+Skeleton skel3 = new Skeleton("Скелет с пистолетом", 30, 7, 0, true);
+Skeleton skel_boss = new Skeleton("Древний скелет", (35 * 2), (3 * 1.3), (2 * 1.4), true);
+Magician mag1 = new Magician("Маг земли", 25, 3, 3, 10);
+Magician mag2 = new Magician("Атакующий маг", 32, 3.5, 2, 10);
+Magician mag3 = new Magician("Защищённый маг", 20, 1.5, 5, 10);
+Magician mag_boss = new Magician("Архимаг 'Геннадий'", (32 * 2), (3.5 * 1.6), (3 * 1.1), (10 * 2));
+
+List<Item> items = new List<Item> { i_heal, i_weapon_1, i_weapon_2, i_weapon_3, i_weapon_4, i_weapon_5, i_weapon_6,
                  i_armor_1, i_armor_2, i_armor_3, i_armor_4, i_armor_5, i_armor_6 };
 
-
-
-List<Enemy> commons = new List<Enemy> { };
-List<Enemy> bosses = new List<Enemy> { };
+List<Enemy> commons = new List<Enemy> { gob1, gob2, gob3, skel1, skel2, skel3, mag1, mag2, mag3 };
+List<Enemy> bosses = new List<Enemy> { gob_boss, skel_boss, mag_boss };
 
 Player play = start();
+
+
+
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+chest();
+
+
+
 
 do
 {
@@ -52,24 +99,29 @@ Player start()
 //крит. шанс/заморозка, величина блока 70–100%)
 //определялись генератором случайных чисел.
 
-void turn()
+void room()
 {
     Random rnd = new Random();
     if (rnd.Next(0, 2) == 1) { chest(); } // 50/50 враг/сундук
     else { fight(false); }
 
-    if (turn_count % 10 == 0) { fight(true); } // каждые 10 шагов - босс
+    if (room_count % 10 == 0) { fight(true); } // каждые 10 шагов - босс
 
-    turn_count++;
+    room_count++;
 }
 
 void fight(bool is_boss)
 {
+    Random rnd = new Random();
     if (is_boss)
     {
-        Random rnd = new Random();
+        
 
         //Console.WriteLine($"Вы встретили босса {}!");
+    }
+    else
+    {
+
     }
 //    -Игрок всегда ходит первым.
 //- Ход игрока: выбрать Атаку или Защиту.
@@ -87,17 +139,44 @@ void fight(bool is_boss)
 void chest()
 {
     Console.WriteLine("Вы наткнтулись на сундук");
-
+    Random random = new Random();
+    int sel_item = random.Next(0, items.Count() - 1);
+    Console.WriteLine($"Вы получили предмет '{items[sel_item].Name}'");
+    if (items[sel_item].Type == Type_e.Heal)
+    {
+        if (items.Count() == 1)
+        {
+            play.hp += 25;
+            Console.WriteLine("Ваш запас HP был пополнен на 1/4!");
+        }
+        else
+        {
+            play.hp = 100;
+            Console.WriteLine("Ваше HP стало максимальным!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Хотите забрать предмет? (да/нет)");
+        string temp = Console.ReadLine();
+        if (temp == "да")
+        {
+            if (items[sel_item].Type == Type_e.Weapon) { play.weapon = items[sel_item]; }
+            if (items[sel_item].Type == Type_e.Armor) { play.armor = items[sel_item]; }
+        }
+        items.Remove(items[sel_item]);
+    }
+        
 //    Из сундука может выпасть лечебное зелье,
 //оружие или доспех(случайно).
 
-//-Лечебное зелье мгновенно полностью лечит игрока.
-//- При выпадении оружия или доспеха нужно:
+    //-Лечебное зелье мгновенно полностью лечит игрока.
+    //- При выпадении оружия или доспеха нужно:
 
-//    -Показать характеристики нового предмета
-//    и текущей экипировки.
-//-Дать выбор: взять новый предмет(заменив текущий)
-//или выбросить его.
+    //    -Показать характеристики нового предмета
+    //    и текущей экипировки.
+    //-Дать выбор: взять новый предмет(заменив текущий)
+    //или выбросить его.
 }
 enum Type_e { Heal, Weapon, Armor }
 class Item
@@ -105,10 +184,11 @@ class Item
     public Type_e Type;
     public string Name;
     public string Description;
+    public bool Is_deleted;
 
     public Item(Type_e type, string name, string description)
     {
-        Type = type; Name = name; Description = description;
+        Type = type; Name = name; Description = description; Is_deleted = false;
     }
 }
 
@@ -180,9 +260,9 @@ class Goblin : Enemy
         : base(name, hp, attack, defense)
     {
         this.name = name;
-        this.hp = 100;
-        this.attack = 5;
-        this.defense = 4;
+        this.hp = 20;
+        this.attack = 2;
+        this.defense = 2;
         this.crit = 10;
     }
     public override void Print()
