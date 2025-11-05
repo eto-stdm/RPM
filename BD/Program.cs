d6d1e74d5a432386282e4b0dc996267d9c91b7d8
eto-stdm <potatocuteuwu@gmail.com>
2025-11-05 15:39:47 +0300

    написан buy_in_shop(), украшения

--- BD/Program.cs
+++ BD/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Security.Cryptography.Pkcs;
 using System.Text;
@@ -16,6 +17,10 @@ namespace BD
             //Core.Context.
 
 
+            //var d = from i in details // стиль SQL
+            //        where i.Detail_id == id
+            //        select i;
+
 
             // добавление денег и деталей (мб сразу в бд сделать)
             List<Details> details = Core.Context.Details.ToList();
@@ -25,16 +30,22 @@ namespace BD
 
             bool flag_game_over = false;
             bool car_fixed = false;
+            Console.WriteLine("---------------------------------------------");
             Console.WriteLine("Вам досталась автомастерская от вашего деда.");
             Console.WriteLine("Вы решили, что вы хотите работать здесь...");
             Console.WriteLine("...До тех пор, пока у вас не кончатся деньги.");
-            Console.WriteLine("\r\n\r\n  ____                                 _        __  \r\n / ___|__ _ _ __  _ __ ___ _ __   __ _(_)_ __   \\ \\ \r\n| |   / _` | '__|| '__/ _ \\ '_ \\ / _` | | '__| (_) |\r\n| |__| (_| | |   | | |  __/ |_) | (_| | | |     _| |\r\n \\____\\__,_|_|___|_|  \\___| .__/ \\__,_|_|_|    (_) |\r\n            |_____|       |_|                   /_/ \r\n\r\n");            
+            Console.WriteLine("\r\n\r\n  ____                                 _        __  \r\n / ___|__ _ _ __  _ __ ___ _ __   __ _(_)_ __   \\ \\ \r\n| |   / _` | '__|| '__/ _ \\ '_ \\ / _` | | '__| (_) |\r\n| |__| (_| | |   | | |  __/ |_) | (_| | | |     _| |\r\n \\____\\__,_|_|___|_|  \\___| .__/ \\__,_|_|_|    (_) |\r\n            |_____|       |_|                   /_/ \r\n\r\n");
+            Console.WriteLine("---------------------------------------------");
 
             while (true)
             {
                 string client_detail = client_arrives();
+
                 while (car_fixed == false)
                 {
+                    foreach (var item in repair) { if (item.Balance <= 0) { flag_game_over = true; end(); break; } }
+                    if (flag_game_over == true) { break; }
+
                     Console.WriteLine("1. Обслужить клиента");
                     Console.WriteLine("2. Закупить детали");
                     Console.WriteLine("3. Проверить баланс и детали");
@@ -49,9 +60,8 @@ namespace BD
                         case "0": return;
                         default: break;
                     }
-
-                    if (flag_game_over) { end(); break; }
                 }
+                if (flag_game_over == true) { break; }
             }
 
             string client_arrives()
@@ -59,10 +69,6 @@ namespace BD
                 Random rnd = new Random();
                 int id = rnd.Next(1, 21);
                 Details selected = Core.Context.Details.First(x => x.Detail_id == id);
-
-                //var d = from i in details // стиль SQL
-                //        where i.Detail_id == id
-                //        select i;
 
                 Console.WriteLine("К вам приехал клиент!");
                 Console.WriteLine($"У него сломан {selected.Detail_name}");
@@ -102,43 +108,74 @@ namespace BD
             }
             void buy_in_shop()
             {
-                foreach (var item in repair) { Console.WriteLine($"Ваш баланс: {item.Balance}"); }
+                foreach (var item in repair) { Console.WriteLine($"Ваш баланс: {item.Balance} рублей"); }
 
-                Console.WriteLine("Выберите деталь для покупки:");
+                Console.WriteLine("Введите ID детали для покупки:");
+                Console.WriteLine("**************************************************************");
                 foreach (var item in details)
                 {
                     Console.WriteLine($"ID детали: {item.Detail_id}, Название: {item.Detail_name}, Цена: {item.Detail_price};");
                 }
-                int detail_c = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("**************************************************************");
+                int buy_detail;
+                try
+                { 
+                    buy_detail = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Введено не число");
+                    return;
+                }
 
-                if (detail_c == 4) // если id детали в списке
+                Details selected = new Details();
+                try
+                {
+                    selected = Core.Context.Details.First(x => x.Detail_id == buy_detail);
+                }
+                catch
+                {
+                    Console.WriteLine("Неверный индекс");
+                }
+
+                if (buy_detail == selected.Detail_id) // если id детали в списке
                 {
                     Console.WriteLine("Укажите количество деталей:");
-                    int am = Convert.ToInt32(Console.ReadLine());
-                    if (am <= 0) { Console.WriteLine("Количество деталей не может быть меньше одной!"); }
+                    int amount;
+                    try { amount = Convert.ToInt32(Console.ReadLine()); }
+                    catch { Console.WriteLine("Введено не число"); return; }
+
+                    if (amount <= 0) { Console.WriteLine("Количество деталей не может быть меньше одной!"); }
                     else
                     {
-                        if (detail_c == 23) // если денег достаточно для покупки
-                        {
-                            Console.WriteLine($"Было куплено {am} шт {detail_c}"); // количество, имя детали
+                        foreach (var item in repair)
+                        { 
+                            if (item.Balance >= buy_detail) // если денег достаточно для покупки
+                            {
+                                item.Balance -= selected.Detail_price * amount;
+                                // + детали
+                                Console.WriteLine($"Было куплено {amount} шт {selected.Detail_name}"); // количество, имя детали
+                                Console.WriteLine($"Текущий баланс: {item.Balance} рублей");
+                            }
+                            else { Console.WriteLine("Недостаточно денег для покупки!"); }
                         }
-                        else { Console.WriteLine("Недостаточно денег для покупки!"); }
                     }
                 }
-                else { Console.WriteLine("Неверный индекс"); }
             }
 
             void check_my_items()
             {
-                foreach (var item in repair) { Console.WriteLine($"Баланс: {item.Balance}"); }
+                foreach (var item in repair) { Console.WriteLine($"Баланс: {item.Balance} рублей"); }
                 foreach (var item in repair_details) { Console.WriteLine($"Название детали: {item.Detail_id}, Количество: {item.Amount};"); }
             }
 
             void end()
             {
+                Console.WriteLine("-----------------------------------------------------");
                 Console.WriteLine("Вы потратили все свои сбережения, пока чинили машинки");
                 Console.WriteLine("Кажется, пора идти на завод.");
                 Console.WriteLine("\r\n\r\n  ____                         ___                 \r\n / ___| __ _ _ __ ___   ___   / _ \\__   _____ _ __ \r\n| |  _ / _` | '_ ` _ \\ / _ \\ | | | \\ \\ / / _ \\ '__|\r\n| |_| | (_| | | | | | |  __/ | |_| |\\ V /  __/ |   \r\n \\____|\\__,_|_| |_| |_|\\___|  \\___/  \\_/ \\___|_|   \r\n\r\n");
+                Console.WriteLine("-----------------------------------------------------");
             }
         }
     }
