712da6019e91b1bb7a4cd7d288901813fad983d0
eto-stdm <potatocuteuwu@gmail.com>
2025-11-06 10:08:08 +0300

    написана логика принятия заказа, buy_in_shop() через исключения, украшения !!!покупка/продажа деталей не отображается до перезагрузки

--- BD/Program.cs
+++ BD/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Cryptography.Pkcs;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Data.Entity.Infrastructure.Design.Executor;
 
 namespace BD
 {
@@ -18,26 +19,26 @@ namespace BD
 
             bool flag_game_over = false;
             bool car_fixed = false;
-            Console.WriteLine("---------------------------------------------");
             Console.WriteLine("Вам досталась автомастерская от вашего деда.");
             Console.WriteLine("Вы решили, что вы хотите работать здесь...");
             Console.WriteLine("...До тех пор, пока у вас не кончатся деньги.");
             Console.WriteLine("\r\n\r\n  ____                                 _        __  \r\n / ___|__ _ _ __  _ __ ___ _ __   __ _(_)_ __   \\ \\ \r\n| |   / _` | '__|| '__/ _ \\ '_ \\ / _` | | '__| (_) |\r\n| |__| (_| | |   | | |  __/ |_) | (_| | | |     _| |\r\n \\____\\__,_|_|___|_|  \\___| .__/ \\__,_|_|_|    (_) |\r\n            |_____|       |_|                   /_/ \r\n\r\n");
-            Console.WriteLine("---------------------------------------------");
 
             while (true)
             {
-                string client_detail = client_arrives();
-
+                Details client_detail = client_arrives();
+                car_fixed = false;
                 while (car_fixed == false)
                 {
                     foreach (var item in repair) { if (item.Balance <= 0) { flag_game_over = true; end(); break; } }
                     if (flag_game_over == true) { break; }
-
+                    Console.WriteLine("============================");
                     Console.WriteLine("1. Обслужить клиента");
                     Console.WriteLine("2. Закупить детали");
                     Console.WriteLine("3. Проверить баланс и детали");
                     Console.WriteLine("0. Выход");
+                    Console.WriteLine("============================");
+
                     string choice = Console.ReadLine();
 
                     switch (choice)
@@ -52,54 +53,75 @@ namespace BD
                 if (flag_game_over == true) { break; }
             }
 
-            string client_arrives()
+            Details client_arrives()
             {
                 Random rnd = new Random();
                 int id = rnd.Next(1, 21);
                 Details selected = Core.Context.Details.First(x => x.Detail_id == id);
 
+                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
                 Console.WriteLine("К вам приехал клиент!");
-                Console.WriteLine($"У него сломан {selected.Detail_name}");
-                return selected.Detail_name;
+                Console.WriteLine($"У него сломан {selected.Detail_name}!");
+                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                return selected;
             }
 
-            void fix_car(string client_detail)
+            void fix_car(Details client_detail)
             {
-                
+                Console.WriteLine("********************************");
                 Console.WriteLine("Принять или отказать в починке?");
                 Console.WriteLine("1. Принять");
                 Console.WriteLine("2. Отказать");
+                Console.WriteLine("********************************");
                 string c = Console.ReadLine();
                 if (c == "1")
                 {
                     Console.WriteLine("Вы приняли заказ.");
-                    if (client_detail == ) // есть деталь
+                    try
                     {
+                        Car_repair_details selected = Core.Context.Car_repair_details.First(x => x.Detail_id == client_detail.Detail_id);
+                        if (selected.Amount == 1) 
+                        { 
+                            Core.Context.Car_repair_details.Remove(selected); 
+                            Core.Context.SaveChanges(); 
+                        }
+                        else
+                        {
+                            selected.Amount -= 1;
+                            Core.Context.SaveChanges();
+                        }
 
-                        //Core.Context.User.Remove();
-                        // удаление детали из инвентаря автосервиса
-                        Console.WriteLine($"Вы производите ремонт {client_detail}."); // название детали
-
-                        foreach (var item in repair) { item.Balance += 3000; }
-                        Console.WriteLine($"Вы получили {c} рублей."); // + на счету
+                        Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++");
+                        Console.WriteLine($"Вы производите ремонт {client_detail.Detail_name}."); // название детали
+                        decimal sum = Convert.ToDecimal(client_detail.Detail_price) + Convert.ToDecimal(1000);
+                        foreach (var item in repair) { item.Balance += sum; }
+                        Console.WriteLine($"Вы получили {sum} рублей."); // + на счету
+                        car_fixed = true;
+                        Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++");
                     }
-                    else // нет детали
+                    catch // нет детали
                     {
                         Random rnd = new Random();
                         Car_repair_details selected = Core.Context.Car_repair_details.First();
                         Core.Context.Car_repair_details.Remove(selected);
 
+                        Console.WriteLine("--------------------------------------------------");
                         Console.WriteLine("У вас нет подходящей детали.");
                         Console.WriteLine("Вы ставите случайную деталь на отвали.");
                         foreach (var item in repair) { item.Balance -= 100000; }
                         Console.WriteLine("Это замечают и вы платите штраф в размере 100000 рублей."); // - на счету
+                        car_fixed = true;
+                        Console.WriteLine("--------------------------------------------------");
                     }
                 }
                 else
                 {
+                    Console.WriteLine("--------------------------------------------------");
                     Console.WriteLine("Вы отказались от заказа.");
                     foreach (var item in repair) { item.Balance -= 3000; }
                     Console.WriteLine("Вы выплачиваете штраф за отказ в обслуживании в размере 3000 рублей.");
+                    car_fixed = true;
+                    Console.WriteLine("--------------------------------------------------");
                 }
             }
 
@@ -136,18 +158,31 @@ namespace BD
                         { 
                             if (item.Balance >= buy_detail) // если денег достаточно для покупки
                             {
-                                foreach (var i in repair_details)
+                                try
                                 {
-                                    if (i.Detail_id == selected.Detail_id)
+                                    item.Balance -= selected.Detail_price * amount;
+                                    Car_repair_details new_detail = new Car_repair_details
                                     {
-                                        i.Amount += 1;
-                                    }
-
+                                        Car_repair_id = 1,
+                                        Detail_id = selected.Detail_id,
+                                        Amount = amount,
+                                    };
+                                    Core.Context.Car_repair_details.Add(new_detail);
+                                    Core.Context.SaveChanges();
                                 }
-                                item.Balance -= selected.Detail_price * amount;
-                                // + детали
+                                catch
+                                {
+                                    foreach (var i in repair_details)
+                                    {
+                                        if (i.Detail_id == selected.Detail_id)
+                                        {
+                                            i.Amount += 1;
+                                        }
+                                    }
+                                }
                                 Console.WriteLine($"Было куплено {amount} шт {selected.Detail_name}."); // количество, имя детали
                                 Console.WriteLine($"Текущий баланс: {item.Balance} рублей.");
+
                             }
                             else { Console.WriteLine("Недостаточно денег для покупки!"); }
                         }
@@ -157,6 +192,9 @@ namespace BD
 
             void check_my_items()
             {
+                Core.Context.SaveChanges();
+                Console.WriteLine("****************************************");
+                Console.WriteLine("Предметы автосервиса:");
                 foreach (var item in repair) { Console.WriteLine($"Баланс: {item.Balance} рублей."); }
                 foreach (var item in repair_details) 
                 {
@@ -168,6 +206,7 @@ namespace BD
                         }
                     }
                 }
+                Console.WriteLine("****************************************");
             }
 
             void end()
