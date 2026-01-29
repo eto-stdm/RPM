4c73704051050bafe6acba81764e1d8c7fc7749e
eto-stdm <potatocuteuwu@gmail.com>
2026-01-29 10:14:38 +0300

    готовый и красивый ItemsPage (визуально), добавление класса для бд прошлый коммит относился к ItemsPage

--- Wpf_13/Pages/ItemsPage.xaml.cs
+++ Wpf_13/Pages/ItemsPage.xaml.cs
@@ -24,9 +24,40 @@ namespace Wpf_13
         public ItemsPage()
         {
             InitializeComponent();
+            
+            List<Items> items = Core.Context.Items.ToList();
 
-            List<String> products = new List<string> { "\\Pics\\nuggets.png", "\\Pics\\ice-cream.png", "\\Pics\\waffle.png", "\\Pics\\fried-chicken.png", "\\Pics\\spaghetti.png", "\\Pics\\burrito.png", "\\Pics\\burger.png", "\\Pics\\junk-food.png", "\\Pics\\croissant.png", "\\Pics\\healthy-food.png" };
-            Products_LB.ItemsSource = products;
+            //List<String> products = new List<string>();
+            //List<String> name = new List<string>();
+            //List<int> price = new List<int>();
+
+            List<Itm> im = new List<Itm>();
+
+            foreach (Items i in items)
+            {
+                Itm test = new Itm(i.Picture, i.Name, i.Price);
+                im.Add(test);
+            }
+            //Name_TB.ItemsSource = name;
+
+            Products_LB.ItemsSource = im;
+
+            // рабочее
+            //List<String> products = new List<string> { "\\Pics\\nuggets.png", "\\Pics\\ice-cream.png", "\\Pics\\waffle.png", "\\Pics\\fried-chicken.png", "\\Pics\\spaghetti.png", "\\Pics\\burrito.png", "\\Pics\\burger.png", "\\Pics\\junk-food.png", "\\Pics\\croissant.png", "\\Pics\\healthy-food.png" };
+            //Products_LB.ItemsSource = products;
+        }
+        
+        public class Itm
+        {
+            public string Picture { get; set; }
+            public string Name { get; set; }
+            public int Price { get; set; }
+            public Itm(string Picture, string Name, int Price)
+            {
+                this.Picture = Picture;
+                this.Name = Name;
+                this.Price = Price;
+            }
         }
     }
 }
