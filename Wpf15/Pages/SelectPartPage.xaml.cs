c81ff4095ba720393b9370a2f9b36203d2b32304
eto-stdm <potatocuteuwu@gmail.com>
2026-02-18 14:48:27 +0300

    сортировка и поиск в selectpartpage

--- Wpf15/Pages/SelectPartPage.xaml.cs
+++ Wpf15/Pages/SelectPartPage.xaml.cs
@@ -20,20 +20,70 @@ namespace Wpf15.Pages
     /// </summary>
     public partial class SelectPartPage : Page
     {
+        int manufSel = 0;
+        List<basepart> baseparts = Core.Context.basepart.ToList();
+        List<manufacturer> manufacturers = Core.Context.manufacturer.ToList();
         public SelectPartPage()
         {
             InitializeComponent();
+            List<string> manufName = new List<string>();
 
-            List<basepart> baseparts = Core.Context.basepart.ToList();
-            List<manufacturer> manufacturers = Core.Context.manufacturer.ToList();
+            foreach (manufacturer m  in manufacturers)
+            {
+                manufName.Add(m.name);
+            }
+            manufName.Add("(нет)");
+            manufName.Sort();
+
             PartsLB.ItemsSource = baseparts;
+            ManufCB.ItemsSource = manufName;
         }
-
 
         private void AddToAssembly_Btn_Click(object sender, RoutedEventArgs e)
         {
             var senderBtn = sender as Button;
             basepart newit = senderBtn.DataContext as basepart;
+        }
+
+        private void SerachBtn_Click(object sender, RoutedEventArgs e)
+        {
+            baseparts = Core.Context.basepart.ToList();
+            string search = SearchTB.Text.ToLower();
+            if (manufSel == 0)
+            {
+                baseparts = Core.Context.basepart.ToList();
+                baseparts = baseparts.Where(part => part.name.ToLower().Contains(search)).ToList();
+
+            }
+            else
+            {
+                baseparts = baseparts.Where(part => part.manufacturerid == manufSel && part.name.ToLower().Contains(search)).ToList();
+            }
+            PartsLB.ItemsSource = baseparts;
+        }
+
+        private void ManufCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            baseparts = Core.Context.basepart.ToList();
+            string search = SearchTB.Text.ToLower();
+
+            ComboBox ManufCB = sender as ComboBox;
+            string tempstr = ManufCB.SelectedItem as string;
+            if (tempstr == "(нет)" && search == "")
+            {
+                manufSel = 0;
+            }
+            else if (tempstr == "(нет)" && search == "")
+            {
+                //manufSel = manufacturers.First(man => man.name == tempstr).id;
+                baseparts = baseparts.Where(part => part.name.ToLower().Contains(search)).ToList();
+            }
+            else if (tempstr != "(нет)")
+            {
+                manufSel = manufacturers.First(man => man.name == tempstr).id;
+                baseparts = baseparts.Where(part => part.manufacturerid == manufSel && part.name.ToLower().Contains(search)).ToList();
+            }
+            PartsLB.ItemsSource = baseparts;
         }
     }
 }
