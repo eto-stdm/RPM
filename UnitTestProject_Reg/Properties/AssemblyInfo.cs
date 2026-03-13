a339af294e14f9962e748589eac68e3f0deac981
FledBuzzard896 <sobaken334@gmail.com>
2026-03-13 12:01:54 +0300

    сделан testunit проект для регистрации (доделать RegTestSucces и рефакторинг)

---
+++ UnitTestProject_Reg/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject_Reg")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject_Reg")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b66d56a8-6956-4726-9d78-3d2f9fbb4c5d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject_Reg/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Wpf_14.Pages;
+
+namespace UnitTestProject_Reg
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RegTestSuccess()
+        {
+        }
+
+        [TestMethod]
+        public void RegTestFail() 
+        {
+            //var page = new RegistratonPage();
+            //Assert.IsFalse(page.Registr("", "", "", "", "");
+            //Assert.IsFalse(page.Registr("test", "test", "test", "test", "123");
+            //Assert.IsFalse(page.Registr("ifgfosajorgjposdjkgposdkfjpogkpsdokghpowerkhksrklhp[sekh[kl[pwelr", "jfadklhfsahgkjsahgkjhsafkjghkjsdhgkjfhskjghksfjdghkljsadfoghaokglka", "f", "f", "13-07-2007");
+        }
+
+    }
+}
