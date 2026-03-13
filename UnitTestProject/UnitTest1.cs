876cc884ba6adeed1c00341ee9dd6a675102c06a
FledBuzzard896 <sobaken334@gmail.com>
2026-03-13 10:52:44 +0300

    Создан проект unit-test, подключены ссылочки :)

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using Wpf_14.Pages;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AuthTest()
+        {
+            var page = new AuthorizationPage();
+        }
+    }
+}
