35460efd36589a273761cf8ae0767e61afe277b8
FledBuzzard896 <sobaken334@gmail.com>
2026-03-13 11:13:08 +0300

    Доделать положительный Unit-Test для авторизации (взять данные из бд)

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,17 +1,27 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Net;
+using System.Windows.Controls;
 using Wpf_14.Pages;
 
-namespace UnitTestProject
+namespace UnitTestProject_Auth
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void AuthTest()
+        public void AuthTestSuccess() 
+        {
+        
+        }
+
+        [TestMethod]
+        public void AuthTestFail() 
         {
             var page = new AuthorizationPage();
+            Assert.IsFalse(page.Auth("", ""));
+            Assert.IsFalse(page.Auth("ewe", "123"));
+            Assert.IsFalse(page.Auth("qwe", "ewe"));
         }
     }
 }
